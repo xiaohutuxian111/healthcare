683a632a161a36aef11fe4538faa2cf1f9d7d007
wei.wang <2398391057@qq.com>
2025-03-24 20:41:22 +0800

    初始化项目骨架

--- backend/app/config/get_db.py
+++ backend/app/config/get_db.py
@@ -1,8 +1,6 @@
 from backend.app.config.database import AsyncSessionLocal, async_engine, Base
 from backend.app.utils.log_util import logger
-import asyncio
-
-from model import  *
+from backend.app.src.entity import *
 
 
 async def get_db():
@@ -27,6 +25,5 @@ async def init_create_table():
         await conn.run_sync(Base.metadata.create_all)
     logger.info('数据库连接成功')
 
-
-if __name__ == '__main__':
-    asyncio.run(init_create_table())
+# if __name__ == '__main__':
+#     asyncio.run(init_create_table())
